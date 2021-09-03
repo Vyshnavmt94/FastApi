fbb608d4447c2f8c29e63ff3fdfe80dfb16fa7d1
Vyshnavmt94 <vyshnav94.mec@gmail.com>
2021-09-03 17:58:00 +0530

    query_param_validation

---
+++ fastapi_tutorials/query_param_validation.py
@@ -0,0 +1,37 @@
+from typing import Optional, List
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(
+    q: Optional[str] = Query(
+        None,
+        alias="item-query",
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+        max_length=50,
+        regex="^fixedquery$",
+        deprecated=True,
+    )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items1/")
+async def read_items(q: Optional[List[str]] = Query(None)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+if __name__ == "__main__":
+    import os
+
+    os.system("uvicorn query_param_validation:app --reload")
